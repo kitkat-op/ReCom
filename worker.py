244a7e89d92d31f1467655b11e56551875d844b5
kitkat-op <72545617+kitkat-op@users.noreply.github.com>
2026-07-04 00:05:54 +0530

    Added worker and scheduler

---
+++ worker.py
@@ -0,0 +1,91 @@
+import requests
+import math
+
+MASTER_URL = "http://13.51.237.154:8000"
+
+worker_name = input("Enter worker name: ")
+
+
+# ==========================
+# PRIME CHECK
+# ==========================
+
+def is_prime(n):
+
+    if n < 2:
+        return False
+
+    for i in range(2, int(math.sqrt(n)) + 1):
+
+        if n % i == 0:
+            return False
+
+    return True
+
+
+# ==========================
+# REGISTER
+# ==========================
+
+register_response = requests.post(
+    f"{MASTER_URL}/register",
+    json={
+        "name": worker_name
+    }
+)
+
+print(register_response.json())
+
+
+# ==========================
+# GET TASK
+# ==========================
+
+task_response = requests.get(
+    f"{MASTER_URL}/get_task"
+)
+
+task = task_response.json()
+
+if "start" not in task:
+
+    print("No task available")
+    exit()
+
+start = task["start"]
+end = task["end"]
+
+print(f"\nReceived task: {start} -> {end}")
+
+
+# ==========================
+# COMPUTE
+# ==========================
+
+count = 0
+
+for num in range(start, end + 1):
+
+    if is_prime(num):
+        count += 1
+
+print(f"Prime Count = {count}")
+
+
+# ==========================
+# SUBMIT RESULT
+# ==========================
+
+result = {
+    "worker": worker_name,
+    "count": count
+}
+
+submit_response = requests.post(
+    f"{MASTER_URL}/submit_result",
+    json=result
+)
+
+print(submit_response.json())
+
+print("\nTask Completed Successfully")
